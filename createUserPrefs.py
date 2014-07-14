7079f8942f1742b66278cc5d6ee905edc445e93c
azureuser <azureuser@leonidas.leonidas.a1.internal.cloudapp.net>
2014-07-14 16:12:09 +0000

    Limit the number of docs in the pipeline

--- createUserPrefs.py
+++ createUserPrefs.py
@@ -3,9 +3,19 @@ from pymongo import MongoClient;
 if __name__ == "__main__":
 	c = MongoClient('localhost', 27017);
 	db = c['mydb'];
-	# a = db.adverts.distinct('campaign');
-	b = db.adverts.aggregate([
-		{"$group": {"_id" : "$os", "timePerOs": {"$min" : "$timestamp"}}},
-		{"$group": {"_id":1, "min": {"$min": "$timePerOs"}}}
-	]);
-	print b;
+	a = db.adverts.distinct('campaign');
+	print a[0]
+#	for campaign in a:
+#		print campaign;
+	db.command({
+		"aggregate":"adverts",
+		"pipeline":[
+			{"$limit": 10000},
+			{"$group": {"_id" : "$user_id", "campaings": {"$push" : "$campaign"}}}
+		],
+		"allowDiskUse":True
+	});
+#	b = db.adverts.aggregate(
+#		[{"$group": {"_id" : "$user_id", "campaings": {"$push" : "$campaign"}}}]
+#		);
+#	print b[0];
